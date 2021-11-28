33ca8261803ec7655dc7a7fed5c070c5f6c6589b
Rajattiwari20 <rajattiwari2098@gmail.com>
2021-11-28 15:32:00 +0530

    Updated the state by adding two reducers (itemsReducer and cartReducer) and expoted rootReducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,24 @@
-const initialState = {}
+import { combineReducers } from 'redux'
 
-export default function rootReducer(state = initialState, actions) {
+const initialItemsState = []
+export function itemsReducer(state = initialItemsState, actions) {
   switch (actions.type) {
     default:
       return state
   }
 }
+
+const initialCartState = []
+export function cartReducer(state = initialCartState, actions) {
+  switch (actions.type) {
+    default:
+      return state
+  }
+}
+
+const rootReducer = combineReducers({
+  items: itemsReducer,
+  cart: cartReducer,
+})
+
+export default rootReducer
